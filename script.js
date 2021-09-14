6cb74d424c8a880e015f46e4748dbcca3ed128f8
Bruno <bruno.rp45@gmail.com>
2021-09-14 00:39:34 -0300

    Votação funcionando

--- script.js
+++ script.js
@@ -34,17 +34,47 @@ function comecarEtapa() {
 }
 
 function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=> {
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if(candidato.length > 0) {
+        candidato = candidato [0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome} <br/>Partido: ${candidato.partido}`;
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+            fotosHtml += ` <div class="d-1-image" >
+                                <img src="images/${candidato.fotos[i].url}" alt="">
+                                ${candidato.fotos[i].legenda}
+                            </div> `;
+        }
 
-}
+        lateral.innerHTML = fotosHtml;
+    }
+
+};
 
 function clicou(n) {
     let elNumero = document.querySelector('.numero.pisca');
     if(elNumero !==null) {
         elNumero.innerHTML = n;
+        // Atribui o número digitado
         numero = `${numero}${n}`;
 
         elNumero.classList.remove('pisca');
-        elNumero.nextElementSibling.classList.add('pisca');
+        // Verifica se chegou no final da digitação
+        if(elNumero.nextElementSibling !== null) {
+            // Adiciona o pisca ao próximo quadrado e permite digitar
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
     }
 };
 
